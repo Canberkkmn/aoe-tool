d0b35b00b521ed64321220003dc5f01dca058e23
ck <canberkkaman@gmail.com>
2023-06-10 02:59:53 +0300

    finish day

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,7 +1,17 @@
+import { CardMedia } from '@mui/material';
 import Layout from '../layout';
 
 function Home() {
-  return <Layout> Home </Layout>;
+  return (
+    <Layout>
+      <CardMedia
+        key={'home-page-card-media'}
+        component="img"
+        loading="lazy"
+        image="http://apod.nasa.gov/apod/image/1502/HDR_MVMQ20Feb2015ouellet1024.jpg"
+      />
+    </Layout>
+  );
 }
 
 export default Home;
