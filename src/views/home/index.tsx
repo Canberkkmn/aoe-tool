bf9fa8bbeb3696c80bfbaac1df7da32ae19a2905
ck <canberkkaman@gmail.com>
2023-06-10 12:51:54 +0300

    add home imh

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,15 +1,23 @@
-import { CardMedia } from '@mui/material';
+import { Box, CardMedia } from '@mui/material';
 import Layout from '../layout';
+
+const imagePath = '../../../public/Images/Age-of-banner.jpg';
 
 function Home() {
   return (
     <Layout>
-      <CardMedia
-        key={'home-page-card-media'}
-        component="img"
-        loading="lazy"
-        image="http://apod.nasa.gov/apod/image/1502/HDR_MVMQ20Feb2015ouellet1024.jpg"
-      />
+      <Box
+        sx={{
+          padding: '56px',
+        }}
+      >
+        <CardMedia
+          key={'home-page-card-media'}
+          component="img"
+          loading="lazy"
+          image={imagePath}
+        />
+      </Box>
     </Layout>
   );
 }
